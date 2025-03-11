eee7f7c6cb6cb0363728ad49ae9e054386947f76
cyzhao <cyzhao@stu.xjtu.edu.cn>
2025-03-11 18:49:11 +0800

    update requirements (AE p2)

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,32 +1,25 @@
 # The commands I used to create my environment
 
-conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
+# dependency of Wand
+sudo apt-get install libmagickwand-dev
+
+# the conda environment
+conda create -n titim python=3.11
+conda activate titim
+
+conda install pytorch torchvision pytorch-cuda=12.1 -c pytorch -c nvidia
 conda install tqdm
 pip install wand
 conda install scikit-learn
 conda install matplotlib
 pip install seaborn
-# conda install -c pytorch torchtext
 pip install h5py
-# pip install adversarial-robustness-toolbox
-# pip install cleverhans
 pip install scikit-image
 pip install tabulate
 pip install opencv-python
 pip install lpips
-pip install shap
 pip install umap-learn
 pip install opentsne
 pip install grad-cam
-# pip install bchlib
-
-# pip install gdown
-# pip install ipywidgets
-
-
-
-
-
-
-
+pip install gdown
 
